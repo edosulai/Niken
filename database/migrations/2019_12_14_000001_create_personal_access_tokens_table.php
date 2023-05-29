adcf0edc72e3d36b12ef6b8ed4e99594f1ab2be2
Edo Sulaiman <edosulaii@gmail.com>
2023-05-29 20:27:44 +0700

    ubah 191 jadi 25

--- database/migrations/2019_12_14_000001_create_personal_access_tokens_table.php
+++ database/migrations/2019_12_14_000001_create_personal_access_tokens_table.php
@@ -16,9 +16,9 @@ return new class extends Migration
         Schema::create('personal_access_tokens', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('tokenable_id');
-            $table->string('tokenable_type', 191);
+            $table->string('tokenable_type', 25);
             $table->index(['tokenable_type', 'tokenable_id']);
-            $table->string('name');
+            $table->string('name', 25);
             $table->string('token', 64)->unique();
             $table->text('abilities')->nullable();
             $table->timestamp('last_used_at')->nullable();
--- database/migrations/2023_01_18_083706_create_employers_table.php
+++ database/migrations/2023_01_18_083706_create_employers_table.php
@@ -15,15 +15,15 @@ return new class extends Migration {
         Schema::create('employers', function (Blueprint $table) {
             $table->id();
             $table->string('nik');
-            $table->string('nama');
-            $table->string('p_area');
-            $table->string('personnel_subarea');
-            $table->string('bagian');
-            $table->string('job_group');
-            $table->string('posisi_kerja');
-            $table->string('status');
+            $table->string('nama', 25);
+            $table->string('p_area', 25);
+            $table->string('personnel_subarea', 25);
+            $table->string('bagian', 40);
+            $table->string('job_group', 25);
+            $table->string('posisi_kerja', 50);
+            $table->string('status', 25);
             $table->enum('jenis_kelamin', ['Male', 'Female']);
-            $table->string('edu_establishment');
+            $table->string('edu_establishment', 25);
             $table->date('tgl_lahir');
             $table->date('tgl_masuk');
             $table->integer('jarak_alamat');
